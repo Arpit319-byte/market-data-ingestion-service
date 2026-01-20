51708cdb990276dbf605366136dea5fe377649df
Arpit Chauhan <arpitchauhan@Arpits-MacBook-Air.local>
2026-01-20 17:46:20 +0530

    First Commit

---
+++ src/main/java/com/example/stockanalyzer/marketdata/StockAnalyzerMarketDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockanalyzer.marketdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyzerMarketDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalyzerMarketDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stockanalyzer/marketdata/StockAnalyzerMarketDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockanalyzer.marketdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalyzerMarketDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
