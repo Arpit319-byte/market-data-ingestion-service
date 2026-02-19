9e156b407b623c279a2bca5d65f5c6ece88d9115
Arpit319-byte <arpitchauhanupal@gmail.com>
2026-02-20 01:42:37 +0530

    Added the CompanyFinancial entity

---
+++ src/main/java/com/example/stockanalyzer/marketdata/entites/CompanyFinancial.java
@@ -0,0 +1,81 @@
+package com.example.stockanalyzer.marketdata.entites;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "company_financial")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CompanyFinancial extends BaseModel {
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id", nullable = false)
+    private Stock stock;
+
+    // Financial period info
+    @Enumerated(EnumType.STRING)
+    private FinancialPeriodType periodType; // QUARTERLY or YEARLY
+
+    private Integer year;
+
+    private Integer quarter; // 1,2,3,4 (null for yearly)
+
+    private LocalDate periodStartDate;
+
+    private LocalDate periodEndDate;
+
+    // Income statement
+    private BigDecimal revenue;
+
+    private BigDecimal netProfit;
+
+    private BigDecimal operatingProfit;
+
+    private BigDecimal eps;
+
+    // Balance sheet
+    private BigDecimal totalAssets;
+
+    private BigDecimal totalLiabilities;
+
+    private BigDecimal totalEquity;
+
+    private BigDecimal debt;
+
+    // Cash flow
+    private BigDecimal operatingCashFlow;
+
+    private BigDecimal freeCashFlow;
+
+    // Ratios
+    private BigDecimal roe;
+
+    private BigDecimal debtToEquity;
+
+    private BigDecimal peRatio;
+
+    private BigDecimal pbRatio;
+    
+}
