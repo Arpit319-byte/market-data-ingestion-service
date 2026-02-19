273edaf2651674ded762b2af6aa3bf52e01c019c
Arpit319-byte <arpitchauhanupal@gmail.com>
2026-02-20 01:45:56 +0530

    Removed the unwanted imports

--- src/main/java/com/example/stockanalyzer/marketdata/entites/CompanyFinancial.java
+++ src/main/java/com/example/stockanalyzer/marketdata/entites/CompanyFinancial.java
@@ -1,16 +1,12 @@
 package com.example.stockanalyzer.marketdata.entites;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.util.UUID;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
